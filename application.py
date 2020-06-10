6264d03eb4101f2943a1bb98ce52f19b4a4ca5dd
GambetaClub <65199352+GambetaClub@users.noreply.github.com>
2020-06-10 10:52:36 -0400

    Second Commit
    Instead of creating objects I am just appending users and channels in a list.

--- application.py
+++ application.py
@@ -7,31 +7,8 @@ app.secret_key = "hello"
 Channels = []
 Users = []
 
-class User:
-    def __init__(self, name, channel):
-        self.name = name
-        self.channel = channel
+Channels.append("Channel 1")
 
-class Message:
-    def __init__(self, author, message):
-        self.author = author
-        self.message = message
-
-class Channel:
-    def __init__(self, index, name):
-        self.index = index
-        self.name = name
-        self.messages = []
-        self.users = []
-
-    def add_message(self, message: Message):
-        self.messages.append(message)
-
-    def add_user(self, user: User):
-        self.users.append(user)
-
-Channel1 = Channel(index=0,name="First Channel")
-Channels.append(Channel1)
 
 @app.route("/")
 def home():
@@ -41,13 +18,12 @@ def home():
 def login():
     if request.method == "POST":
         session.permanent = True
-        user = request.form["username"]
-        for user in Users:
-            if user in Users:
-                return render_template("error.html", message="A user with that username is already logged in :(")
+        user = request.form.get("username")
+        if user in Users:
+            return render_template("error.html", message="A user with that username is already logged in :(")
         session["user"] = user
-        new_user = User(name=user,channel=Channel1)
-        Users.append(new_user)
+        Users.append(user)
+        pickle.dump(user, open( "save.p", "wb" ))
         return redirect(url_for("user"))
     else:
         if "user" in session:
@@ -76,8 +52,6 @@ def logout():
     else:
             return render_template("error.html", message="You are not logged in :(")
         
-
-
 @app.route("/channels", methods={"GET"})
 def channels_view():
     if session.get("user"):
@@ -98,13 +72,12 @@ def channel():
 def create_channel():
     if request.method == "GET":
         return render_template("create_channel.html")
-    if channel_name in Channels:
-        return render_template("error.html", message="A channel with taht username is already created")
-    channel_name = request.form.get("channel_name")
-    channel_index= len(Channels)
-    new_channel = Channel(index=channel_index, name=channel_name)
-    Channels.append(new_channel)
-    return render_template("channel.html", channel= channels)
+    else:
+        channel_name = request.form.get("channel_name")
+        if channel_name in Channels:
+            return render_template("error.html", message="A channel with that name is already created")
+        Channels.append(channel_name)
+        return render_template("channel.html", channel=channel_name)
 
 
 if __name__ == "__main__":
